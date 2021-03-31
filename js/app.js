16a8c952e5df87ee26a1440ac3a98fc3e3a84c14
alejandro medici <el.aleyo@gmail.com>
2021-03-30 21:46:32 -0300

    ultimos cambios

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 
-const persona1 = new Persona("Ale", "Medici", 25);
+const persona1 = new Persona("", "Medici", 25);
 
 console.log(persona1.nombre);
 console.log(persona1.apellido);
 console.log(persona1.edad);
 console.log(persona1.email);
+
+
+const persona2 = new Persona("Alejandro", "M", 25, "ale@mail.com");
+
+if (persona2.nombre == persona1.nombre) {
+    
+}
--- js/persona.js
+++ js/persona.js
@@ -1,6 +1,6 @@
 function Persona(pNombre, pApellido, pEdad, pEmail = "") {
-    this.nombre = pNombre;
     this.apellido = pApellido;
     this.edad = pEdad;
+    this.nombre = pNombre;
     this.email = pEmail; // garantizo que al menos va a tener el valor por defecto
 }
